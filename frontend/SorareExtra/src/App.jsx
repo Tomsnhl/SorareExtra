651b48d788672c95d3a4cc478d3ce63b4198265b
bruly2 <b.bertogal@gmail.com>
2023-08-08 20:13:01 +0200

    1ere HP

--- frontend/SorareExtra/src/App.jsx
+++ frontend/SorareExtra/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ballon from "./assets/ballon.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={ballon} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+            <h1>Sorare Card Battle Royale</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Sorare Card Battle Royale : Dans ce mode de jeu, les utilisateurs s'affrontent dans des matchs de style "Battle Royale" où le dernier joueur restant gagne. Chaque utilisateur a un "deck" de cartes Sorare et utilise les attributs de ces cartes pour "combattre" les autres utilisateurs.
+                </p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
